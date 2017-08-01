d9ba08cd58801e41a5fc1037e3de13d037cf1e47
Jacob Perrine <jacob.perrine@pcc.edu>
2017-07-31 18:56:29 -0700

    added language field to Country class

--- src/edu/pcc/cis233j/countries/Country.java
+++ src/edu/pcc/cis233j/countries/Country.java
@@ -1,14 +1,24 @@
 package edu.pcc.cis233j.countries;
 
+import java.util.ArrayList;
+
 /**
  * A country in the world
- * @author Your Name & Cara Tang
+ * @author Jacob Perrine & Cara Tang
+ * @version 2017.07.31
+ *
+ * @modifications
+ * - Added ArrayList<String> language as a field. Included the import statement.
+ * - Initialized language as a null list in the class constructor.
+ * - Created addLanguage() to add languages to the language ArrayList.
+ * - Added a getter, getLanguage(), to access a country's language(s).
  */
 public class Country {
 	private int id;
 	private String name;
 	private long population;
 	private double medianAge;
+	private ArrayList<String> language;
 	
 	/**
 	 * Create a Country object with the given properties
@@ -18,6 +28,23 @@ public class Country {
 		this.name = name;
 		this.population = population;
 		this.medianAge = medianAge;
+		this.language = new ArrayList<>();
+	}
+
+	/**
+	 * Add a language to a country
+	 * @param language The language to be added
+	 */
+	public void addLanguage(String language) {
+		this.language.add(language);
+	}
+
+	/**
+	 * @return a list of the languages spoken in a country, or NULL if none
+	 * have been assigned.
+	 */
+	public ArrayList<String> getLanguage() {
+		return language;
 	}
 
 	public int getId() {
